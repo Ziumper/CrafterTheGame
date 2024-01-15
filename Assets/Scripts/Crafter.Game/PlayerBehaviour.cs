00fe7eb602336c5cb6ad39a53ecc5678fa4ab23a
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-01-15 20:19:58 +0100

    [Cleanup] Cleanup after the import
    Move ThirdPersonController into PlayerBehaviour.
    Add litle refactoring
    Remove third person controller and dependencies
    Cleanup scene and change editor version.
    Move assets to seperated folders.

--- Assets/Scripts/Crafter.Game/PlayerBehaviour.cs
+++ Assets/Scripts/Crafter.Game/PlayerBehaviour.cs
@@ -7,16 +7,70 @@ namespace Crafter.Game
 {
     public class PlayerBehaviour : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Header("Movement")]
+        [SerializeField] private float _playerSpeed = 5.0f;
+        [SerializeField] private float _jumpForce = 1.0f;
 
+        [Header("Components")]
+        [SerializeField] private Camera _gameCamera;
+        private CharacterController _controller;
+        private Animator _animator;
+        
+        private Vector3 _playerVelocity;
+        private bool _groundedPlayer;
+        private float _gravityValue = -9.81f;
+
+        private void Start()
+        {
+            _controller = gameObject.GetComponentInChildren<CharacterController>();
+            _animator = gameObject.GetComponentInChildren<Animator>();
         }
 
-        // Update is called once per frame
         void Update()
         {
+            HandleMovement();    
+        }
 
+        public void HandleMovement()
+        {
+            _groundedPlayer = _controller.isGrounded;
+
+            if (_groundedPlayer && _playerVelocity.y < 0)
+            {
+                _playerVelocity.y = -0.5f;
+            }
+
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            //trasnform input into camera space
+            var forward = _gameCamera.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+            var right = Vector3.Cross(Vector3.up, forward);
+
+            Vector3 move = forward * input.z + right * input.x;
+            move.y = 0;
+
+            _controller.Move(move * Time.deltaTime * _playerSpeed);
+
+            _animator.SetFloat("MovementX", input.x);
+            _animator.SetFloat("MovementZ", input.z);
+
+            if (input != Vector3.zero)
+            {
+                gameObject.transform.forward = forward;
+            }
+
+            // Changes the height position of the player..
+            if (Input.GetButtonDown("Jump") && _groundedPlayer)
+            {
+                _playerVelocity.y += Mathf.Sqrt(_jumpForce * -3.0f * _gravityValue);
+                _animator.SetTrigger("Jump");
+            }
+
+            _playerVelocity.y += _gravityValue * Time.deltaTime;
+
+            _controller.Move(_playerVelocity * Time.deltaTime);
         }
     }
 
