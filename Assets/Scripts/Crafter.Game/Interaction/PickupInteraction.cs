f97607b5b555e7f2a80f189ffa17ae981bcf85b4
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-01-16 09:40:19 +0100

    [Import] Integration outline for pickup objects

--- Assets/Scripts/Crafter.Game/Interaction/PickupInteraction.cs
+++ Assets/Scripts/Crafter.Game/Interaction/PickupInteraction.cs
@@ -2,9 +2,25 @@
 {
     public class PickupInteraction : MonoInteraction
     {
+        private Outline _outline;
+
         void Start()
         {
             _interactionName = "Pickup";
+            _outline = GetComponent<Outline>();
+            _outline.enabled = false;
+        }
+
+        public override void Notice()
+        {
+            base.Notice();
+            _outline.enabled = true;
+        }
+
+        public override void Ignore()
+        {
+            base.Ignore();
+            _outline.enabled = false;
         }
 
         public override void Interact(InteractionArgs args)
