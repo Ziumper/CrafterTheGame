df11dba11bdb7c2942e9e0bf37bc7f9e48799d66
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-01-19 07:09:13 +0100

    [feature] show crafting message

--- Assets/Scripts/Crafter.Game/Crafting/CraftingMessageNotice.cs
+++ Assets/Scripts/Crafter.Game/Crafting/CraftingMessageNotice.cs
@@ -1,6 +1,8 @@
 ﻿using Crafter.Game.Equipment;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime.Collections;
 using UnityEngine;
 
 namespace Crafter.Game.Crafting
@@ -8,19 +10,65 @@ namespace Crafter.Game.Crafting
     public class CraftingMessageNotice : MonoBehaviour
     { 
         [SerializeField] private float _waitTime;
-        [SerializeField] private TextMeshProUGUI _item;
+        [SerializeField] private TextMeshProUGUI _message;
+        [SerializeField, TextArea] private string _successText;
+        [SerializeField, TextArea] private string _failedText;
+        [SerializeField, TextArea] private string _noMaterialsText;
 
-        public void ShowNoticeMessage(EquipmentObject equipment)
+        private readonly HashSet<string> _objectsSet = new ();
+        private readonly Queue<string> _objectsQueue = new ();
+        private bool _isPlaying = false;
+
+
+        public void OnSuccess(EquipmentObject equipmentObject)
         {
-            _item.text += equipment.Name;
+            AppendMessageAndShowNotice(_successText, equipmentObject);
+        }
+
+        public void OnFailure(EquipmentObject equipmentObject)
+        {
+            AppendMessageAndShowNotice(_failedText, equipmentObject);
+        }
+
+        public void OnNNoMaterialsText(EquipmentObject equipmentObject)
+        {
+            AppendMessageAndShowNotice(_noMaterialsText, equipmentObject);
+        }
+
+        private void AppendMessageAndShowNotice(string message, EquipmentObject equipment)
+        {
+            var messageToShow = message + equipment.Name;
+            ShowNoticeMessage(messageToShow);
+        }
+
+        private void ShowNoticeMessage(string message)
+        {
+            if(_objectsSet.Contains(message))
+            {
+                return;
+            } else
+            {
+                _objectsSet.Add(message);
+                _objectsQueue.Enqueue(message);
+            }
+            
+            if (_isPlaying) { return; }
             gameObject.SetActive(true);
             StartCoroutine(WaitForEnd());
         }
 
         private IEnumerator WaitForEnd()
         {
-            yield return new WaitForSeconds(_waitTime);
-           gameObject.SetActive(false);
+            _isPlaying = true;
+            while(_objectsQueue.TryDequeue(out var message)) 
+            {
+                _message.text = message;
+                yield return new WaitForSeconds(_waitTime);
+                _objectsSet.Remove(message);
+            }
+
+            _isPlaying = false;
+            gameObject.SetActive(false);
         }
 
     }
